92425c8d6fd9215c50f132a36493aa6e7a5f08a2
Justin Manzano <manzanojustin37@gmail.com>
2026-08-18 22:08:36 +0800

    PR 3:feature/model-department-employeee

---
+++ IT_ELECTIVE_PREFINALS_PROJECT/Models/Depratment.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Models;
+
+[Table("Departments")]
+public class Department
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public int IsActive { get; set; } = 1;
+
+    public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    public ICollection<Team> Teams { get; set; } = new List<Team>();
+}
