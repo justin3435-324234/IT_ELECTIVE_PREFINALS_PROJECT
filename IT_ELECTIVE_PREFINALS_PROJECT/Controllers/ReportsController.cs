eaae37a7724e9bbc5c7c4be168e84f5d492a92ec
kykiehub <cocnigamaliel@gmail.com>
2026-08-19 03:09:24 +0800

    PR 21:feature/reports-controller

---
+++ IT_ELECTIVE_PREFINALS_PROJECT/Controllers/ReportsController.cs
@@ -0,0 +1,25 @@
+﻿using IT_ELECTIVE_PREFINALS_PROJECT.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Controllers;
+
+public class ReportsController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public ReportsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        ViewBag.TotalTickets = await _context.Tickets.CountAsync();
+        ViewBag.UnassignedTicketsCount = await _context.Tickets
+            .Where(t => !t.TicketAssignments.Any())
+            .CountAsync();
+        ViewBag.TotalEmployees = await _context.Employees.CountAsync();
+
+        return View();
+    }
+}
